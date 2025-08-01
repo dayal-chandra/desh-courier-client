10483f0d3f57ca6acd79ef914ca9d3b597d1ae66
dayal chandra <dayalchandra511@gmail.com>
2025-08-01 11:09:21 +0600

    responsive navbar with theme toggling

---
+++ src/routes/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        path: "/",
+        element: <h1>hello cvai</h1>,
+      },
+    ],
+  },
+]);
